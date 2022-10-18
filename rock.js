2de95273d9214a8e51f329bd836814f1f9865d8f
Borakarayel <m.bora.karayel@gmail.com>
2022-10-18 22:10:25 +0300

    Computers choice function created

--- rock.js
+++ rock.js
@@ -1,3 +1,12 @@
+//check everything alright
 console.log("Hello World");
-
 document.write("Hello World!")
+
+
+//Random computer choice creator
+function getComputerChoice(max) {
+return Math.floor(Math.random()*max)};
+let computerMove= getComputerChoice(3);
+if(computerMove === 0) {console.log("Rock")}
+    else if (computerMove === 1){console.log("Paper")}
+    else if (computerMove === 2){console.log("Scissors")};
