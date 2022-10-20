c2146a51c843af0e03ec8f8478d1aebb79ed8232
Borakarayel <m.bora.karayel@gmail.com>
2022-10-20 13:44:14 +0300

    evaluation

--- rock.js
+++ rock.js
@@ -5,17 +5,37 @@
 
 //Random computer choice creator
 function getComputerChoice(max) {
-return Math.floor(Math.random()*max)};
+    return Math.floor(Math.random()*max)};
 let computerMove= getComputerChoice(3);
-if(computerMove === 0) {console.log("Rock")}
+if(computerMove === 0) {computerMove ="Rock"}
     else if (computerMove === 1){
-        console.log("Paper")}
+        computerMove ="Paper"}
     else if (computerMove === 2){
-        console.log("Scissors")};
+        computerMove ="Scissors"};
+
+console.log(computerMove) ;
 
 //Get User choice
 let userChoice = prompt("Please type Rock, Paper or Scissors !")
-userChoice = userChoice.toUpperCase().charAt(0) + 
+userChoice = userChoice.toUpperCase().charAt(0)+
 userChoice.toLowerCase().slice(1);
 
-document.write(userChoice)
+//play a single round
+
+if (userChoice === computerMove) {
+    console.log("Draw!")}
+else 
+{
+if (userChoice === "Rock" && computerMove === "Scissors") {
+    console.log("Won!") }
+else if (userChoice === "Rock" && computerMove === "Paper") {
+    console.log("Lose!") }
+else if (userChoice === "Paper" && computerMove === "Rock") {
+    console.log("Won!") }
+else if (userChoice === "Paper" && computerMove === "Scissors") {
+    console.log("Lose!") }
+else if (userChoice === "Scissors" && computerMove === "Rock") {
+    console.log("Lose!") }
+else if (userChoice === "Scissors" && computerMove === "Paper") {
+    console.log("Won!") }
+    }
