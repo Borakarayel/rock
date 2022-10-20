5c0bdf92d297e0d26ed16faa59469f6e88967559
Borakarayel <m.bora.karayel@gmail.com>
2022-10-20 09:58:45 +0300

    get user choice

--- rock.js
+++ rock.js
@@ -1,6 +1,6 @@
 //check everything alright
-console.log("Hello World");
-document.write("Hello World!")
+//console.log("Hello Warrior");
+//document.write("Hello World!")
 
 
 //Random computer choice creator
@@ -8,5 +8,14 @@ function getComputerChoice(max) {
 return Math.floor(Math.random()*max)};
 let computerMove= getComputerChoice(3);
 if(computerMove === 0) {console.log("Rock")}
-    else if (computerMove === 1){console.log("Paper")}
-    else if (computerMove === 2){console.log("Scissors")};
+    else if (computerMove === 1){
+        console.log("Paper")}
+    else if (computerMove === 2){
+        console.log("Scissors")};
+
+//Get User choice
+let userChoice = prompt("Please type Rock, Paper or Scissors !")
+userChoice = userChoice.toUpperCase().charAt(0) + 
+userChoice.toLowerCase().slice(1);
+
+document.write(userChoice)
